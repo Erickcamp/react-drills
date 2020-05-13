a2c5a09141336f16790cf2914246871424301f3c
Erickcamp <thecampkid@gmail.com>
2020-05-13 14:59:33 -0600

    done

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      dayArr: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    }
+  }
+
+  render(){
+    let week = this.state.dayArr.map((e, index) => {
+      return <h2 key={index}>{e}</h2>
+    })
+      return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {week}
     </div>
   );
+  }
 }
 
 export default App;
